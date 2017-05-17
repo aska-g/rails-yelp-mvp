9fa427267e02c65846cf154cc8d2efb765aaa4f9
Joanna Gaudyn <aska.gaudyn@gmail.com>
2017-05-17 14:29:00 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [ :index, :new, :create ]
+  end
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
+end
